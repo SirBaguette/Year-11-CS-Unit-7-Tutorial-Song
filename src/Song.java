496c174b5fdf211e931a2079b30a41aef5f36a38
SirBaguette <sebastianz2026@student.cis.edu.hk>
2024-05-29 00:26:37 +0800

    .

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,34 @@
-public class Song {
+import java.util.ArrayList;
+import java.util.List;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private List<String> listeners;
+    public Song(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+        this.listeners = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int howMany(List<String> newListeners) {
+        int rs = 0;
+        for (String listener : newListeners) {
+            String list = listener.toLowerCase();
+            if (!listeners.contains(list)) {
+                listeners.add(list);
+                rs++;
+            }
+        }
+        return rs;
+    }
+
 }
